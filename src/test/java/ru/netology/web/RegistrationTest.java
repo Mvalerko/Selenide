821d0e781b3641b879f58c48aed514691b50d911
mvalerko <04445326@mail.ru>
2023-05-08 14:48:43 +0200

    Первая задача выполнена

--- src/test/java/ru/netology/web/RegistrationTest.java
+++ src/test/java/ru/netology/web/RegistrationTest.java
@@ -1,16 +1,14 @@
 package ru.netology.web;
 
 import com.codeborne.selenide.Configuration;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
 
-
 import java.time.Duration;
-import java.util.Date;
 
-import static com.codeborne.selenide.Condition.*;
-import static com.codeborne.selenide.Selectors.*;
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.*;
 
 class RegistrationTest {
@@ -20,9 +18,26 @@ class RegistrationTest {
     //@Disabled
     @Test
     void positiveScriptRegistration() {
-        //Configuration.headless = true;
+        Configuration.headless = true;
         open("http://localhost:9999");
         $("[placeholder='Город']").setValue("Владимир");
+        for(int i = 0; i < 10; i++) {
+            $("[data-test-id='date'] input").sendKeys(Keys.BACK_SPACE);
+        }
+        $("[data-test-id='date'] input").setValue(customData.future(5));
+        $("[name='name']").setValue("Валерко Михаил");
+        $("[name='phone']").setValue("+79032423264");
+        $$("span").find(exactText("Я соглашаюсь с условиями обработки и использования моих персональных данных")).click();
+        $$("button").find(exactText("Забронировать")).click();
+        $(withText("Успешно!")).shouldBe(visible, Duration.ofSeconds(15));
+    }
+
+    @Test
+    void positiveScriptRegistrationInteractive() {
+        Configuration.headless = true;
+        open("http://localhost:9999");
+        $("[placeholder='Город']").setValue("Вл");
+        $$("span").find(exactText("Владикавказ")).click();
         for(int i = 0; i < 10; i++) {
             $("[data-test-id='date'] input").sendKeys(Keys.BACK_SPACE);
         }
